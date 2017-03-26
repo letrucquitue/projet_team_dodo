673823e5b29771326ccb766e37bacd3f57a39694
Hotaruko <cirdecll@hotmail.fr>
2017-03-26 13:03:20 +0200

    admin connexion

--- frontoffice/login_process.php
+++ frontoffice/login_process.php
@@ -7,17 +7,39 @@
   		$user_email = trim($_POST['user_email']);
   		$user_password = trim($_POST['password']);
   		$password = $user_password;
+  		$stmt = mysqli_prepare($conn,"SELECT id, mdp FROM admin WHERE login=?");                 
+      mysqli_stmt_bind_param($stmt, "s", $user_email);
+  		mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);    
+      mysqli_stmt_bind_result($stmt,$row['id'],$row['mdp']);
+      if(mysqli_stmt_num_rows($stmt)>0){
+        mysqli_stmt_fetch($stmt);
+        if($row['mdp']==$password){	
+    		  $_SESSION['user_id'] = $row['id'];
+    	    header("location: admin.php");
+    		  exit;
+        }
+      }
+      unset($stmt);
   		$stmt = mysqli_prepare($conn,"SELECT id, mdp FROM utilisateur WHERE email=? or login=?");
       mysqli_stmt_bind_param($stmt, "ss", $user_email, $user_email);
   		mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
       mysqli_stmt_bind_result($stmt,$row['id'],$row['mdp']);
-      mysqli_stmt_fetch($stmt);
-  		if($row['mdp']==$password){	
-  		  $_SESSION['user_id'] = $row['id'];
-  	    header("location: index.php");
-  		}
+      if(mysqli_stmt_num_rows($stmt)>0){
+        mysqli_stmt_fetch($stmt);
+    		if($row['mdp']==$password){	
+    		  $_SESSION['user_id'] = $row['id'];
+    	    header("location: index.php");
+    		}
+    		else{	
+          echo "email or password does not exist."; // wrong details 
+          header("location: connexion.php"); 
+    		} 
+      }
   		else{	
-  			echo "email or password does not exist."; // wrong details 
+        echo "email or password does not exist."; // wrong details 
+        header("location: connexion.php"); 
   		}
   	}
     else{
